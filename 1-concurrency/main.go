37015dd090c9182c29c3144b8dc09a178ca0e5d4
Unfriendly Monkey <andreysereda@ya.ru>
2025-05-18 12:49:25 +0300

    Deleting old

--- 1-concurrency/main.go
+++ 1-concurrency/main.go
@@ -2,33 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
-var r = rand.New(rand.NewSource(time.Now().UnixNano()))
-
-func numsGenerator(numCh chan int) {
-	for i := 0; i < 10; i++ {
-		numCh <- r.Intn(101)
-	}
-	close(numCh)
-}
-
-func squareNums(numCh chan int, sqCh chan int) {
-	for n := range numCh {
-		res := n * n
-		sqCh <- res
-	}
-	close(sqCh)
-}
-
 func main() {
-	numCh := make(chan int)
-	sqCh := make(chan int)
-
-	go numsGenerator(numCh)
-	go squareNums(numCh, sqCh)
 
 	// Add timeout to prevent infinite waiting
 	timeout := time.After(2 * time.Second)
